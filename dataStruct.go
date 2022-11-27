f816ba0112c6bce527550137309bcc566217a6d4
gyupilsim <garleyqp@gmail.com>
2022-11-27 23:13:41 +0900

    데이터 저장부분 합치는 작업 추가

--- dataStruct.go
+++ dataStruct.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
 
 //필요 설정 항목
 ///내 정보
@@ -8,6 +12,8 @@ import "time"
 ///성능 정보
 
 type SettingAgent struct {
+	filePath        string
+	fileName        string
 	KafkaServerAddr string
 	KafkaServerPort string
 	HeaderKeyCode   string
@@ -22,8 +28,58 @@ type PerfInterval struct {
 	CoreUtil int
 }
 
-func NewSetting() *SettingAgent {
+func (asetting *SettingAgent) Save() {
+	jsonEncode, err := json.Marshal(asetting)
+	if err != nil {
+		panic(err)
+	} else {
+		nfile, err := os.Create(asetting.GetSavePath())
+		if err != nil {
+			panic(err)
+		}
+		nfile.Write(jsonEncode)
+		defer nfile.Close()
+	}
+}
+
+func (asetting *SettingAgent) Load() {
+	savePath := asetting.GetSavePath()
+	_, err := os.Stat(savePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err := os.Mkdir(asetting.filePath, os.ModeAppend)
+			if err != nil {
+				panic(err)
+			}
+			file, err := os.Create(savePath)
+			if err != nil {
+				panic(err)
+			}
+
+			jsonEncode, err := json.Marshal(asetting)
+			if err != nil {
+				panic(err)
+			}
+			file.Write(jsonEncode)
+			defer file.Close()
+		}
+	}
+
+	ontuneSettingData, err := os.Open(savePath)
+	defer ontuneSettingData.Close()
+	jsonDecoder := json.NewDecoder(ontuneSettingData)
+	jsonDecoder.Decode(&asetting)
+	asetting.Save()
+}
+
+func (asetting *SettingAgent) GetSavePath() string {
+	return asetting.filePath + asetting.fileName
+}
+
+func initSetting(basePath string, filename string) *SettingAgent {
 	asetting := SettingAgent{}
+	asetting.filePath = basePath
+	asetting.fileName = filename
 	asetting.KafkaServerAddr = "127.0.0.1"
 	asetting.KafkaServerPort = "9092"
 	asetting.HeaderKeyCode = "testAgent"
@@ -36,6 +92,12 @@ func NewSetting() *SettingAgent {
 	asetting.GetPerfDataTime.CoreUtil = 5
 
 	return &asetting
+}
+
+func NetSetting(basePath string, filename string) *SettingAgent {
+	asetting := initSetting(basePath, filename)
+	asetting.Load()
+	return asetting
 }
 
 type HostAgentInfo struct {
